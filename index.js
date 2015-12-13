5639ac3e367ca3a0d3e43dbfad14ebaf4ea99d9d
Pawel Chomicki <chomicki.pawel@gmail.com>
2015-12-12 19:06:48 -0500

    Version bump. Put API key in settings

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 var https = require('https');
 
+var options = {};
+
+exports.setApiKey = function(apiKey) {
+    options.apiKey = apiKey;
+};
+
+exports.getApiKey = function() {
+    return options.apiKey;
+};
+
 exports.heartbeat = function() {
     https.get('https://www.stockfighter.io/ob/api/heartbeat', function(res) {
         console.log("statusCode: ", res.statusCode);
