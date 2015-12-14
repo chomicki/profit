3cd7ae3fb25d691d25c4cd296e9822ba95e29b80
Pawel Chomicki <chomicki.pawel@gmail.com>
2015-12-13 20:41:37 -0500

    Mocks are starting to work. Writing tests.

--- test/test.js
+++ test/test.js
@@ -1,26 +1,53 @@
-var sinon = require('sinon');
+var nock = require('nock');
 var assert = require('assert');
 var profit = require('../index');
 
 describe('StockFighterApiWrapper', function () {
 
-	describe('#getHeartbeat()', function () {
+	describe('API Key Tests', function(){
+		it('should initially be null', function () {
+			assert.equal(profit.getApiKey(),null);
+		});
 
+		it('should should be updated by the set method', function () {
+			var key = "TESTKEY";
+			profit.setApiKey(key);
+			assert.equal(profit.getApiKey(),key);
+		});
+	});
+
+	describe('Application Heartbeat Tests', function(){
+		it('should respond when it alive', function(done){
+			nock('https://www.stockfighter.io')
+			.get('/ob/api/heartbeat')
+			.reply(
+				200,
+				{
+					"ok": true,
+					"error": ""
+				}
+			);
+			profit.getAppHeartbeat(function(d) {
+				assert.equal(d.ok, true);
+				assert.equal(d.code, 200);
+				assert.equal(d.error, "")
+				done();
+			});
+
+		});
+
+		// should send
+
+	});
+/*
+	describe('#getHeartbeat()', function () {
 		it('should return ok when the app is alive', function () {
 			var cb = sinon.stub().returns(23);
 			profit.setApiKey('2');
 			var proxy = profit.getHeartbeat(cb);
 			console.log(proxy);
 			assert.equal(proxy,23,"should be 23");
-		})
+		});
 	});
-
-	describe('subtractOne()', function () {
-
-	it('should correctly subtract one from the given number', function () {
-	  // assertions here
-	})
-
-	})
-
-})
+*/
+});
